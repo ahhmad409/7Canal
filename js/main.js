7c8f82d79ca43cd647a613b67b00de60e8abd209
Ahmad Hameed <ahhmad409@gmail.com>
2022-09-18 16:12:09 +0500

    some fixes and improvements
    media page  added, booking inquiery page added, whatsapp chat option added, logo changed, contact sidebar customized

--- js/main.js
+++ js/main.js
@@ -14,15 +14,15 @@ const allLinks = document.querySelectorAll(".downloadLink").forEach((link) => {
   link.addEventListener("click", () => {
     const downloadButton = document.getElementById("downloadBtn");
 
-    const a = document.createElement("a");
-    a.textContent = "Submit & Download";
-    a.classList.add("btn", "btn-primary");
-    a.setAttribute("id", "downloadBtn");
-    a.setAttribute("type", "button");
-    a.setAttribute("href", "#");
-    document.querySelector(".modal-footer").replaceChild(a, downloadButton);
+    // const a = document.createElement("a");
+    // a.textContent = "Submit & Download";
+    // a.classList.add("btn", "btn-primary");
+    // a.setAttribute("id", "downloadBtn");
+    // a.setAttribute("type", "button");
+    // a.setAttribute("href", "#");
+    // document.querySelector(".modal-footer").replaceChild(a, downloadButton);
 
-    a.addEventListener("click", () => {
+    downloadButton.addEventListener("click", () => {
       const name = document.getElementById("inputName");
       const email = document.getElementById("inputEmail");
       const contact = document.getElementById("inputContact");
@@ -30,7 +30,7 @@ const allLinks = document.querySelectorAll(".downloadLink").forEach((link) => {
       console.log(`Name: ${name} \nEmail: ${email} \nContact: ${contact}`);
 
       if (!name.value || !email.value || !contact.value) {
-        a.setAttribute("href", "#");
+        downloadButton.setAttribute("href", "#");
         Swal.fire({
           title: "Please Enter All Fields",
           confirmButtonText: "OK",
@@ -42,7 +42,7 @@ const allLinks = document.querySelectorAll(".downloadLink").forEach((link) => {
         /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
       if (email.value.match(validEmail)) {
       } else {
-        a.setAttribute("href", "#");
+        downloadButton.setAttribute("href", "#");
 
         Swal.fire({
           title: "Please check your email again!",
@@ -54,7 +54,7 @@ const allLinks = document.querySelectorAll(".downloadLink").forEach((link) => {
       const validPhone = "^([0-9()/+ -]*)$";
       if (contact.value.match(validPhone)) {
       } else {
-        a.setAttribute("href", "#");
+        downloadButton.setAttribute("href", "#");
 
         Swal.fire({
           title: "Please check your phone number!",
@@ -67,17 +67,21 @@ const allLinks = document.querySelectorAll(".downloadLink").forEach((link) => {
 
       // FOR PRODUCTION
       if (link.textContent == "Brochure")
-        a.setAttribute("href", "./downloads/7Canal Brochure.pdf");
+        downloadButton.setAttribute("href", "./downloads/7Canal Brochure.pdf");
       else if (link.textContent == "3D Walkthrough")
-        a.setAttribute("href", "./downloads/7-Canal-walkthrough.mp4");
+        downloadButton.setAttribute(
+          "href",
+          "./downloads/7-Canal-walkthrough.mp4"
+        );
       else if (link.textContent == "Price Lists") {
-        a.setAttribute("download", "Price List");
-        a.setAttribute("href", "./downloads/price-list.jpeg");
+        downloadButton.setAttribute("download", "Price List");
+        downloadButton.setAttribute("href", "./downloads/price-list.jpeg");
       } else if (link.textContent == "Construction Update")
-        a.setAttribute("href", "#");
-      else if (link.textContent == "Layouts") a.setAttribute("href", "#");
+        downloadButton.setAttribute("href", "#");
+      else if (link.textContent == "Layouts")
+        downloadButton.setAttribute("href", "#");
       else if (link.textContent == "Floor Plans")
-        a.setAttribute("href", "./downloads/floor-plans.pdf");
+        downloadButton.setAttribute("href", "./downloads/floor-plans.pdf");
 
       // FOR DEMO
       // if (link.textContent == "Brochure")
@@ -107,80 +111,119 @@ $(document).ready(function () {
   });
 });
 
-document.getElementsByClassName('book_now_bottom_section')[0].addEventListener('mouseenter', () => {
-  document.getElementsByClassName('booking_form')[0].style.transform = "scaleX(1)"
-})
-document.getElementsByClassName('book_now_widget book_now_wrapper')[0].addEventListener('mouseleave', () => {
-  document.getElementsByClassName('booking_form')[0].style.transform = "scaleX(0)"
-})
-let arrowBtn = document.getElementById('book_now_arrow');
-arrowBtn.addEventListener('mouseenter', () => {
-  document.getElementsByClassName('booking_form')[0].style.transform = "scaleX(0)"
+document
+  .getElementsByClassName("book_now_bottom_section")[0]
+  .addEventListener("mouseenter", () => {
+    document.getElementsByClassName("booking_form")[0].style.transform =
+      "scaleX(1)";
+  });
 
-})
-arrowBtn.addEventListener('click', () => {
-  let widget = document.getElementsByClassName('book_now_bottom_section')[0];
-  if (widget.classList.contains('book_now_widget_hidden')) {
-    widget.style.left = '0px'
-    arrowBtn.innerText = '←'
+document
+  .getElementsByClassName("book_now_widget book_now_wrapper")[0]
+  .addEventListener("mouseleave", () => {
+    document.getElementsByClassName("booking_form")[0].style.transform =
+      "scaleX(0)";
+  });
+
+let arrowBtn = document.getElementById("book_now_arrow");
+// arrowBtn.addEventListener("mouseenter", () => {
+//   document.getElementsByClassName("booking_form")[0].style.transform =
+//     "scaleX(0)";
+// });
+
+arrowBtn.addEventListener("click", () => {
+  let widget = document.getElementsByClassName("book_now_bottom_section")[0];
+  if (widget.classList.contains("book_now_widget_hidden")) {
+    widget.style.left = "0px";
+    arrowBtn.innerText = "←";
+  } else {
+    widget.style.left = "-50px";
+    arrowBtn.innerText = "→";
   }
-  else {
-    widget.style.left = '-50px'
-    arrowBtn.innerText = '→'
-  }
-  widget.classList.toggle('book_now_widget_hidden')
+  widget.classList.toggle("book_now_widget_hidden");
 });
+
 function onFormSubmit(event) {
   event.preventDefault();
-  document.getElementsByClassName('booking_form')[0].style.transform = "scaleX(0)"
+  const name = document.getElementById("book-now-name");
+  const phone = document.getElementById("book-now-phone");
+  const email = document.getElementById("book-now-email");
+  const option = document.getElementById("book-now-dropdown");
+  const message = document.getElementById("book-now-message");
+
+  console.log("name", name.value);
+  console.log("phone", phone.value);
+  console.log("email", email.value);
+  console.log("option", option.value);
+  console.log("message", message.value);
+
+  name.value = "";
+  phone.value = "";
+  email.value = "";
+  option.value = "";
+  message.value = "";
+
+  Swal.fire({
+    title: "Your Query has been recieved",
+    confirmButtonText: "OK",
+  });
+
+  document.getElementsByClassName("booking_form")[0].style.transform =
+    "scaleX(0)";
 }
 
-let downloadArrowButton = document.getElementById('download_widgets_arrow');
+let downloadArrowButton = document.getElementById("download_widgets_arrow");
 
-downloadArrowButton.addEventListener('click', () => {
-  let widget = document.getElementsByClassName('downloads_widget_bottom_section')[0];
-  if (widget.classList.contains('book_now_widget_hidden')) {
-    widget.style.right = '0px'
-    downloadArrowButton.innerText = '→'
+downloadArrowButton.addEventListener("click", () => {
+  let widget = document.getElementsByClassName(
+    "downloads_widget_bottom_section"
+  )[0];
+  if (widget.classList.contains("book_now_widget_hidden")) {
+    widget.style.right = "0px";
+    downloadArrowButton.innerText = "→";
+  } else {
+    widget.style.right = "-160px";
+    downloadArrowButton.innerText = "←";
   }
-  else {
-    widget.style.right = '-160px'
-    downloadArrowButton.innerText = '←'
-  }
-  widget.classList.toggle('book_now_widget_hidden');
+  widget.classList.toggle("book_now_widget_hidden");
 
-  let downloadList = document.getElementById('downloads_list');
-  if (!downloadList.classList.contains('opened')) {
-    downloadList.style.left = '0rem';
+  let downloadList = document.getElementById("downloads_list");
+  if (!downloadList.classList.contains("opened")) {
+    downloadList.style.left = "0rem";
+  } else {
+    downloadList.style.left = "-10rem";
   }
-  else {
-    downloadList.style.left = '-10rem';
-  }
-  downloadList.classList.toggle('opened');
+  downloadList.classList.toggle("opened");
 });
 
-document.getElementById('sidebarButton').addEventListener('click', (event) => {
-  let sidebar = document.getElementById('right-sidebar');
-  sidebar.style.right = '0px'
-})
-document.getElementById('sidebar_closeButton').addEventListener('click', (event) => {
-  let sidebar = document.getElementById('right-sidebar');
-  sidebar.style.right = '-400px'
-})
-document.getElementById('widget_downloads').addEventListener('click', () => {
-  let downloadList = document.getElementById('downloads_list');
-  if (!downloadList.classList.contains('opened')) {
-    downloadList.style.left = '-10rem';
+document.getElementById("widget_downloads").addEventListener("click", () => {
+  let downloadList = document.getElementById("downloads_list");
+  if (!downloadList.classList.contains("opened")) {
+    downloadList.style.left = "-10rem";
+  } else {
+    downloadList.style.left = "7rem";
   }
-  else {
-    downloadList.style.left = '7rem';
-  }
-  downloadList.classList.toggle('opened');
-})
-document.getElementById('book_now_bottom_section').addEventListener('click',()=>{
-  let downloadList = document.getElementById('downloads_list');
-  if (downloadList.classList.contains('opened')) {
-    downloadList.style.left = '7rem';
-    downloadList.classList.remove('opened');
-  }
-})
+  downloadList.classList.toggle("opened");
+});
+
+document
+  .getElementById("book_now_bottom_section")
+  .addEventListener("click", () => {
+    let downloadList = document.getElementById("downloads_list");
+    if (downloadList.classList.contains("opened")) {
+      downloadList.style.left = "7rem";
+      downloadList.classList.remove("opened");
+    }
+  });
+
+document.getElementById("sidebarButton").addEventListener("click", (event) => {
+  let sidebar = document.getElementById("right-sidebar");
+  sidebar.style.right = "0px";
+});
+
+document
+  .getElementById("sidebar_closeButton")
+  .addEventListener("click", (event) => {
+    let sidebar = document.getElementById("right-sidebar");
+    sidebar.style.right = "-400px";
+  });
